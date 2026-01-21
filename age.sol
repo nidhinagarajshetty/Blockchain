2b9829e538ab625afcc84e84d82fe8a0e97055b6
nidhinagarajshetty <nidhishetty035@gmail.com>
2026-01-21 10:51:20 +0530

    Add files via upload

---
+++ age.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity>=0.8.2<0.9.0;
+contract vote{
+    uint public age;
+    string public result;
+    function checkvote(uint _age) public{
+        age = _age;
+        if(age >= 18){
+            result = "Eligible";
+        }else{
+            result = "Not Eligible";
+        }
+    }
+}
+    
